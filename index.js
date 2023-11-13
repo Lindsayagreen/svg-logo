cff5bf90db928e6f254a14a3369be28d803f1448
lindsayagreen <lindsgree@gmail.com>
2023-11-12 20:07:18 -0600

    added code

---
+++ index.js
@@ -0,0 +1,76 @@
+// constants to pull in required inquirer module and shape + svg objects
+const  inquirer = require("inquirer");
+const fs = require('fs');
+const {Circle, Triangle, Square} = require("./lib/shapes.js");
+const {writeFile} = require("fs/promises");
+const SVG = require("./lib/shapes.test.js");
+
+
+// function uses inquirer to ask user for desired logo characteristics
+function createLogo() {
+inquirer
+    .prompt([
+    {
+        type: "input",
+        name: "charsLogo",
+        message: "Enter up to 3 characters for your logo",
+        validate: charsInput => {
+            if (charsInput) {
+                return true;
+            } else {
+                console.log("Enter between 1 and 3 characters")
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "charsColor",
+        message: "Enter color for characters",
+    },
+    {
+        type: "list",
+        name: "shapeType",
+        message: "Pick one of the following shapes for your logo",
+        choices: ["Circle", "Triangle", "Square"]
+    },
+    {
+        type: "input",
+        name: "shapeColor",
+        message: "Enter a color to give the shape more style",
+    },
+
+    ])
+
+    // use logo characteristics to generate svg file
+    .then (({ charsLogo, charsColor, shapeType, shapeColor}) => {
+    var shape;
+    switch (shapeType) {
+        case "Circle":
+        shape = new Circle();
+        break;
+        case "Triangle":
+        shape = new Triangle();
+        break;
+        case "Square":
+        shape = new Square();
+        break;
+    }
+    // generates the new logo
+    shape.setColor(shapeColor);
+    const svg = new SVG();
+    svg.setText(charsLogo, charsColor);
+    svg.setShape(shape)
+    const svgMarkup = svg.render();
+    return writeFile("logo.svg", svgMarkup);
+    })
+    .then(() => {
+    console.log("Success! Logo generated.")
+    })
+    .catch((error) => {
+    console.log("Error!", error)
+    });
+}
+
+// call createLogo function
+createLogo();
